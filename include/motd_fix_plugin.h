6bc58f0a1ec0da06b697d6242e2aa406a12a4383
Dingsel <dshluka@gmail.com>
2024-10-12 12:42:35 +0200

    Logging

--- include/motd_fix_plugin.h
+++ include/motd_fix_plugin.h
@@ -8,8 +8,11 @@ class MOTDFix : public endstone::Plugin {
 public:
 
 	void onServerPingEvent(endstone::ServerListPingEvent& event) {
+        getLogger().debug("Ping Incomming: " + event.getRemoteHost());
+        
         event.setMotd("Origins, Factions, Economy And More");
         event.setLevelName("Tasty Factions");
+
         //Spoofing the port to avoid port mismatch with Proxy <--> Server
         event.setLocalPort(19132);
 	}
